053fb7f5c6de6c7f66fcb489c65cc0a888828b7d
ptrToYuri <ptrToYuri@gmail.com>
2021-11-04 01:58:16 +0300

    Placeholder for the future update recovery functionality

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -3,6 +3,7 @@
 const Agenda = require('agenda');
 
 const interchanges = require('../controllers/interchanges');
+const recover = require('../bot/recover')
 
 const agenda = new Agenda();
 agenda.defaultLockLifetime(30 * 1000);
@@ -15,10 +16,12 @@ Agenda.prototype.createSession = function (db) {
 agenda.on('ready', () => console.log('[SCHEDULER] Synced with database'));
 
 agenda.define('invalidate-timed-out', interchanges.invalidateTimedOut);
+agenda.define('recover-updates', recover);
 
 agenda.on('ready', async () => {
 	try {
 		await agenda.every('*/20 * * * * *', 'invalidate-timed-out');
+		await agenda.every('0 0 * * * *', 'recover-updates');
 	} catch (err) {
 		console.log(`[SCHEDULER] Init failed: ${err.message}`)
 		process.exit(1);
